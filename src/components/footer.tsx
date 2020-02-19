ac4d02dc57c15df3dbdcc569afbe7612a2f8ccb1
Andrew Oliveira <QuiGonSwag@users.noreply.github.com>
2020-02-19 12:42:32 -0500

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,6 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
-<!--       <ExtLink href="https://github.com/ijjk/notion-blog">Source code</ExtLink> -->
     </footer>
   </>
 )
