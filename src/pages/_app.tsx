63a6f09237c71e2b438fbd0cabf52c681682c2e6
Ayush Goyal <ayushg1214@gmail.com>
2020-05-25 21:07:05 +0530

    refactor footer component (#31)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,9 @@
 import '../styles/global.css'
-import ExtLink from '../components/ext-link'
+import Footer from '../components/footer'
 
 export default ({ Component, pageProps }) => (
   <>
     <Component {...pageProps} />
-
-    <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://zeit.co/new/project?template=https://github.com/ijjk/notion-blog/tree/master">
-        <img
-          src="https://zeit.co/button"
-          height={46}
-          width={132}
-          alt="deploy to ZEIT button"
-        />
-      </ExtLink>
-      <span>
-        or{' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
-        </ExtLink>
-      </span>
-    </footer>
+    <Footer />
   </>
 )
