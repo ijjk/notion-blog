658f81798a653fa1110fe48ebef7ec44ecd26f97
Takafumi Komiyama <takafumi.komiyama.35@gmail.com>
2020-05-30 14:57:24 +0900

    add download btn at first

--- src/components/sections/first.tsx
+++ src/components/sections/first.tsx
@@ -15,28 +15,28 @@ export default () => {
               <br />
               <span>生活を。</span>
             </h1>
-            {/* <div className={`${style.section_first_download}`}>
+            <div className={`${style.section_first_download}`}>
               <div className={`${style.section_first_download_text}`}>
                 <p>
                   <span>今すぐダウンロード!</span>
                 </p>
               </div>
               <div className={`${style.section_first_download_icon}`}>
-                <img src="/assets/appstore.svg" height="65px" />
-                <img src="/assets/playstore.svg" height="65px" />
-              </div>
-            </div> */}
-            <div
-              className={`${style.note}`}
-              style={{ backgroundColor: 'rgba(241, 165, 0, 0.7)' }}
-            >
-              <div className={`${style.note_message}`}>
-                現在サービス入れ替えのため、横浜・東京・大宮でサービスを一時休止しています
-              </div>
-              <div className={`${style.note_button}`}>
-                <Link href="/news/service_suspension">
-                  <a>詳細を確認</a>
-                </Link>
+                <a
+                  href="#" // ここにAppStoreリンクを入れる
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mr-2"
+                >
+                  <img src="/assets/appstore.svg" height="65px" />
+                </a>
+                <a
+                  href="#" // ここにGooglePlayリンクを入れる
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/assets/playstore.svg" height="65px" />
+                </a>
               </div>
             </div>
             <div className={`${style.note}`} style={{ marginTop: '10px' }}>
