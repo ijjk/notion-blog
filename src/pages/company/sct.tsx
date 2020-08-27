e984d8dd3db6f491808c7dabdc1d6a6a1415affc
nakar0 <shoutaro425@gmail.com>
2020-08-27 12:00:24 +0900

    refactored head

--- src/pages/company/sct.tsx
+++ src/pages/company/sct.tsx
@@ -1,31 +1,14 @@
-import Head from 'next/head'
 import Title from '../../components/primary/title'
 import style from '../../styles/page.module.scss'
 
+import DefaultHead from '../../components/heads/default'
+
 export default () => (
   <>
-    <Head>
-      <title>特定商取引法に基づく記載 | アイカサ</title>
-      <meta property="og:title" content="お問い合わせ" />
-      <meta property="og:image" content="https://www.i-kasa.com/ogp.jpg" />
-      <meta property="description" content="特定商取引法に基づく記載" />
-      <meta property="og:description" content="特定商取引法に基づく記載" />
-      <meta property="og:url" content="https://www.i-kasa.com" />
-      <meta property="og:type" content="website" />
-      <meta
-        data-hid="twitter:card"
-        name="twitter:card"
-        content="summary_large_image"
-      />
-      <meta data-hid="twitter:site" name="twitter:site" content="@ikasa1111" />
-      <meta
-        data-hid="og:site_name"
-        property="og:site_name"
-        content="アイカサ | 傘のシェアリングサービス"
-      />
-      <meta data-hid="og:locale" property="og:locale" content="ja_JP" />
-      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-    </Head>
+    <DefaultHead
+      titlePre="特定商取引法に基づく記載"
+      description="特定商取引法に基づく記載"
+    ></DefaultHead>
     <Title
       title="特定商取引法に基づく記載"
       description
