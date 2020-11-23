b2d508cea9baa40498ed9d1cd7874c8de8f64f06
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2020-11-23 11:43:30 +0900

    :+1: put google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,35 @@ import '../styles/section.scss'
 import '../components/cards/index_howtouse.scss'
 import '../components/cards/index_keypoint.scss'
 
-import ExtLink from '../components/ext-link'
+import { useEffect } from 'react'
+import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+
 import Footer from '../components/primary/footer'
 import Navbar from '../components/primary/navbar'
 import Download from '../components/primary/download'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Navbar />
-    <Component {...pageProps} />
+import * as gtag from '../lib/gtag'
 
-    <Download />
-    <Footer />
-  </>
-)
+export default ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  })
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+
+      <Download />
+      <Footer />
+    </>
+  )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_ID } from '../lib/gtag'
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}', {
+                page_path: window.location.pathname,
+              });
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
