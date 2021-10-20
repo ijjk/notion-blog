2125d085c1770590954c503a04310e65fd42af76
jj@jjsweb.site <jj@jjsweb.site>
2021-10-20 00:12:15 -0500

    Update queryCollection body

--- src/lib/notion/queryCollection.ts
+++ src/lib/notion/queryCollection.ts
@@ -6,44 +6,31 @@ export default function queryCollection({
   loader = {},
   query = {},
 }: any) {
-  const {
-    limit = 999, // TODO: figure out Notion's way of handling pagination
-    loadContentCover = true,
-    type = 'table',
-    userLocale = 'en',
-    userTimeZone = 'America/Phoenix',
-  } = loader
-
-  const {
-    aggregate = [
-      {
-        aggregation_type: 'count',
-        id: 'count',
-        property: 'title',
-        type: 'title',
-        view_type: 'table',
+  const queryCollectionBody = {
+    loader: {
+      type: 'reducer',
+      reducers: {
+        collection_group_results: {
+          type: 'results',
+          limit: 999,
+          loadContentCover: true,
+        },
+        'table:uncategorized:title:count': {
+          type: 'aggregation',
+          aggregation: {
+            property: 'title',
+            aggregator: 'count',
+          },
+        },
       },
-    ],
-    filter = [],
-    filter_operator = 'and',
-    sort = [],
-  } = query
+      searchQuery: '',
+      userTimeZone: 'America/Phoenix',
+    },
+  }
 
   return rpc('queryCollection', {
     collectionId,
     collectionViewId,
-    loader: {
-      limit,
-      loadContentCover,
-      type,
-      userLocale,
-      userTimeZone,
-    },
-    query: {
-      aggregate,
-      filter,
-      filter_operator,
-      sort,
-    },
+    ...queryCollectionBody,
   })
 }
