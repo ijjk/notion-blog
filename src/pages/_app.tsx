f4e82a5021a32ea2932ccbe146cf9319fb25aaba
sukechannnn <wonderfulworld0315@gmail.com>
2021-11-24 14:06:37 +0900

    Remove header & footer & no-need pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import '../styles/global.css'
 import 'katex/dist/katex.css'
-import Footer from '../components/footer'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Footer />
     </>
   )
 }
