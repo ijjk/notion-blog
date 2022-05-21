a48a8b6bee340c50bca2acc5c9494639e20a40ab
兰天游 <dtlantianyou@gmail.com>
2022-05-21 21:53:32 +0800

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -42,6 +42,7 @@ if (!BLOG_INDEX_ID) {
 }
 
 module.exports = {
+  swcMinify: true,
   webpack(cfg, { dev, isServer }) {
     // only compile build-rss in production server build
     if (dev || !isServer) return cfg
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,21 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <span>Deploy your own!</span>
-        <ExtLink href="https://vercel.com/new/git/external?repository-url=https://github.com/ijjk/notion-blog/tree/main&project-name=notion-blog&repository-name=notion-blog">
-          <img
-            src="https://vercel.com/button"
-            height={46}
-            width={132}
-            alt="deploy to Vercel button"
-          />
-        </ExtLink>
-        <span>
-          or{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog">
-            view source
-          </ExtLink>
-        </span>
+        Hear me roar
       </footer>
     </>
   )
